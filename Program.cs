269ba5835be54d3cb450fe8fea06b08116089260
husmus00 <60274772+husmus00@users.noreply.github.com>
2023-12-31 04:58:27 +0300

    Transferred source files to submodule and made necessary changes to the rest of the project

---
+++ Program.cs
@@ -0,0 +1,62 @@
+using ManoMachine;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+//var builder = WebAssemblyHostBuilder.CreateDefault(args);
+//builder.RootComponents.Add<App>("#app");
+//builder.RootComponents.Add<HeadOutlet>("head::after");
+
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+//await builder.Build().RunAsync();
+
+namespace ManoMachine
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
+
+namespace Interface
+{
+    public class Console
+    {
+        public static void WriteLine()
+        {
+            // Pages.Machine.outputText += Environment.NewLine;
+        }
+
+        public static void WriteLine(string output)
+        {
+            // Pages.Machine.outputText += (output + Environment.NewLine);
+        }
+
+        public static void Write(string output)
+        {
+        }
+
+        public static void Clear()
+        {
+        }
+
+        public static void ReadKey()
+        {
+
+        }
+
+        public static string ReadLine()
+        {
+            return "";
+        }
+    }
+}
